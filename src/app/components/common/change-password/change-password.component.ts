6b099501d23cfbe8a35cf573653f4f9fbe0cdfbd
Jayakrishnank2001 <jkrishnan2001@gmail.com>
2024-03-25 19:08:25 +0530

    Added Settings Page

---
+++ src/app/components/common/change-password/change-password.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { passwordMatchValidator, validateByTrimming } from 'src/app/helpers/validations';
+import { passwordValidators } from 'src/app/shared/validators';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from 'src/app/material/material.module';
+
+
+@Component({
+  selector: 'app-change-password',
+  templateUrl: './change-password.component.html',
+  styleUrls: ['./change-password.component.css'],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, FormsModule, MaterialModule]
+})
+export class ChangePasswordComponent implements OnInit {
+
+  form!: FormGroup
+  hideConfirmPassword:boolean=true
+
+  constructor(private fb: FormBuilder,
+    private dialogRef: MatDialogRef<ChangePasswordComponent>) { }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      newPassword: ['', [validateByTrimming(passwordValidators)]],
+      confirmPassword: ['', Validators.required]
+    }, { validators: passwordMatchValidator })
+  }
+
+
+  onSubmit(): void {
+    this.dialogRef.close(this.form.value)
+  }
+
+  get isFormValid(): boolean {
+    return this.form?.valid &&
+      this.form?.get('newPassword')?.value === this.form?.get('confirmPassword')?.value;
+  }
+
+
+
+
+}
